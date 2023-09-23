2f59cad52d0bf3babf0577e7082ab19ab53b70cc
Michał Hanusek <mhanusek@energotest.com.pl>
2023-09-23 21:31:55 +0200

    Solution 1 - enum/trait

--- src/main.rs
+++ src/main.rs
@@ -105,61 +105,10 @@ mod app {
         }
     }
 
-    // Option 2
-
-    // 1. Frame -> operate ( DO -> set outputs, DI -> set parameters)
-    // 2. Read state -> Frame (Do -> read outputs, DI -> read inputs)
-
-    fn do_process_frame(t: &mut DiDo, frame: u32) {
-        //TODO: decode frame and operate on Do card (set outputs)
-    }
-
-    fn di_process_frame(t: &mut DiDo, frame: u32) {
-        //TODO: decode frame and operate on Di card (e.g. set parameters of filter)
-    }
     
-    fn do_read_state(t: &mut DiDo) -> u32 {
-        0
-    }
-
-    fn di_read_state(t: &mut DiDo) -> u32 {
-        0
-    }
-
-    pub struct DiDo
-    {
-        inputs : Vec<ErasedPin<stm32f1xx_hal::gpio::Input>, 16>,
-        outputs : Vec<ErasedPin<stm32f1xx_hal::gpio::Output>, 16>,
-        read_state_fn: fn(t: &mut DiDo) -> u32,
-        process_frame: fn(t: &mut DiDo, frame: u32)
-    }
-
-    impl DiDo
-    {
-        pub fn new_from(state: bool) -> Self {
-            if state {
-                Self{ 
-                    inputs: Vec::new(), 
-                    outputs: Vec::new(),
-                    read_state_fn: do_read_state,
-                    process_frame: do_process_frame
-                }
-            }
-            else {
-                Self{ 
-                    inputs: Vec::new(), 
-                    outputs: Vec::new(),
-                    read_state_fn: di_read_state,
-                    process_frame: di_process_frame
-                }
-            }
-        }
-    }
-
     #[shared]
     struct Shared {
-        dido: DiDo,
-        // dev_type: DevType,
+        dev_type: DevType,
     }
 
     #[local]
@@ -173,13 +122,7 @@ mod app {
         // DI or DO
         let dev_type_selector = gpioa.pa0.into_pull_down_input(&mut gpioa.crl);
 
-        let dido = DiDo::new_from(dev_type_selector.is_high());
-        // let dt = DevType::new_di();
-        
-        // let dev_type = if dev_type_selector.is_high() { di_read_state } else { do_out };
-        (Shared {dido}, Local {}, init::Monotonics())
-
-        // let dev_type = if dev_type_selector.is_high() { DevType::new_di() } else {  DevType::new_do() };
-        // (Shared {dev_type: dev_type}, Local {}, init::Monotonics())
+        let dev_type = if dev_type_selector.is_high() { DevType::new_di() } else {  DevType::new_do() };
+        (Shared {dev_type}, Local {}, init::Monotonics())
     }
 }
